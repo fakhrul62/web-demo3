2c9cf477427ae6b11c524dc1d3def6e62a757715
Md. Fakhrul Alam Shuvo <ifakhrul23@gmail.com>
2024-04-10 16:51:41 +0600

    added section

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function () {  
+
+let counter = 10
+setInterval(() => {
+		if(counter>0){
+			counter--
+		}
+    document.getElementById('counterElement').style.setProperty('--value', counter)
+}, 1000)
+
+
+});
